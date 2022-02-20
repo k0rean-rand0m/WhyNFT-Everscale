0b85a97b38d21e45ea98f8bc538c5002c725c56a
mike-petrov <mis5116@yandex.ru>
2022-02-20 12:00:51 +0300

    fix

--- frontend/src/Containers/GameEvents.jsx
+++ frontend/src/Containers/GameEvents.jsx
@@ -20,19 +20,19 @@ const GameEvents = ({ onPopup }) => {
             { name: '1 military vehicle destroyed', icon: 'minus', className: 'stat_block_item_title_purple' },
             { name: '2 mining machine destroyed', icon: 'plus', className: 'stat_block_item_title_brown' },
             { name: '1 Industrial building destroyed', icon: 'minus', className: 'stat_block_item_title_metal' },
-            { name: 'Planted 2 earth plants', icon: 'minus', className: 'stat_block_item_title_green' },
-            { name: 'Planted 2 earth plants', icon: 'plus', className: 'stat_block_item_title_green' },
-            { name: 'Planted 2 earth plants', icon: 'plus', className: 'stat_block_item_title_green' },
-            { name: 'Planted 2 earth plants', icon: 'plus', className: 'stat_block_item_title_green' },
-            { name: 'Planted 2 earth plants', icon: 'plus', className: 'stat_block_item_title_green' },
+            { name: 'Planted 7 earth plants', icon: 'minus', className: 'stat_block_item_title_green' },
+            { name: 'Planted 6 earth plants', icon: 'plus', className: 'stat_block_item_title_green' },
+            { name: 'Planted 5 earth plants', icon: 'plus', className: 'stat_block_item_title_green' },
+            { name: 'Planted 4 earth plants', icon: 'plus', className: 'stat_block_item_title_green' },
+            { name: 'Planted 3 earth plants', icon: 'plus', className: 'stat_block_item_title_green' },
             { name: 'Planted 2 earth plants', icon: 'plus', className: 'stat_block_item_title_green' },
           ].map((item) => (
             <div className="stat_block_item" key={item.name}>
-              <FontAwesomeIcon
-                icon={item.icon === 'plus' ? ['far', 'circle-dot'] : ['far', 'circle']}
-                style={{ marginRight: 10 }}
-              />
-              <div className={`stat_block_item_title ${item.className}`}>
+              <div className="stat_block_item_title">
+                <FontAwesomeIcon
+                  icon={item.icon === 'plus' ? ['far', 'circle-dot'] : ['far', 'circle']}
+                  style={item.icon === 'plus' ? { padding: '0 10px', color: '#168c4d' } : { padding: '0 10px' }}
+                />
                 <span>{item.name}</span>
                 <span>{item.count}</span>
               </div>
