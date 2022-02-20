113173f292a4e19c3e8f7aadb5cbd17e0d9fbce8
mike-petrov <mis5116@yandex.ru>
2022-02-20 12:02:05 +0300

    working map & dashboard

--- frontend/src/Containers/Map.jsx
+++ frontend/src/Containers/Map.jsx
@@ -4,97 +4,39 @@ import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-load
 
 mapboxgl.accessToken = 'pk.eyJ1IjoibWlrZS1wZXRyb3YiLCJhIjoiY2t6dWZjM2I5NHVpMTJ2bnlkMnpieW5hcCJ9.ZSXwDnPqVR4UQVo-h7YtfQ';
 
-const Map = ({ onPopup }) => {
+const Map = ({ onPopup, lands }) => {
   const mapContainer = useRef(null);
 
   useEffect(() => {
     const map = new mapboxgl.Map({
       container: mapContainer.current,
       style: 'mapbox://styles/mike-petrov/ckzufxdbc00tp14l8lri1wm3o',
-      center: [31, 23],
+      center: [31.6, 23],
       zoom: 9,
       maxBounds:[[30,22], [32,24]],
     });
-    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right')
+    map.addControl(new mapboxgl.NavigationControl(), 'bottom-right');
+
+    const mapLands = [];
+    for (let i = 0; i < lands.length; i++) {
+      mapLands.push({
+        'type': 'Feature',
+        'properties': {
+          ...lands[i],
+        },
+        'geometry': {
+          'type': 'Polygon',
+          'coordinates': lands[i].coordinates,
+        }
+      });
+    }
 
     map.on('load', () => {
       map.addSource('areas', {
         'type': 'geojson',
         'data': {
           'type': 'FeatureCollection',
-          'features': [{
-            'type': 'Feature',
-            'properties': {
-              'name': 'Bigrows',
-              'color': '#168C4D',
-              'className': 'stat_block_item_title_green'
-            },
-            'geometry': {
-              'type': 'Polygon',
-              'coordinates': [
-                [
-                  [31, 23],
-                  [31.2, 23],
-                  [31.2, 23.2],
-                  [31, 23.2],
-                ]
-              ]
-            }
-          }, {
-            'type': 'Feature',
-            'properties': {
-              'name': 'Flowberies',
-              'color': '#FF0000',
-              'className': 'stat_block_item_title_red'
-            },
-            'geometry': {
-              'type': 'Polygon',
-              'coordinates': [
-                [
-                  [31, 23],
-                  [30.8, 23],
-                  [30.8, 22.8],
-                  [31, 22.8],
-                ]
-              ]
-            }
-          }, {
-            'type': 'Feature',
-            'properties': {
-              'name': 'Lurkoban',
-              'color': '#9D4C00',
-              'className': 'stat_block_item_title_brown'
-            },
-            'geometry': {
-              'type': 'Polygon',
-              'coordinates': [
-                [
-                  [31, 23],
-                  [30.8, 23],
-                  [30.8, 23.2],
-                  [31, 23.2],
-                ]
-              ]
-            }
-          }, {
-            'type': 'Feature',
-            'properties': {
-              'name': 'Olya',
-              'color': '#45ebaf',
-              'className': ''
-            },
-            'geometry': {
-              'type': 'Polygon',
-              'coordinates': [
-                [
-                  [31, 23],
-                  [31, 22.8],
-                  [31.2, 22.8],
-                  [31.2, 23],
-                ]
-              ]
-            }
-          }]
+          'features': mapLands
         }
       });
 
@@ -113,11 +55,7 @@ const Map = ({ onPopup }) => {
       });
 
       map.on('click', 'areas', (e) => {
-        if (e.features[0].properties.name === 'Olya') {
-          onPopup('my');
-        } else {
-          onPopup('attack', e.features[0].properties);
-        }
+        onPopup('attack', e.features[0].properties);
       });
 
       map.on('mouseenter', 'areas', () => {
@@ -130,7 +68,7 @@ const Map = ({ onPopup }) => {
     });
 
     return () => map.remove();
-  }, [onPopup]);
+  }, [lands]);
 
   return (
     <div className="container container_map">
